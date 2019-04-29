daaf7ac4c92709eb84a0e1308777277876b15cbb
tomy0000000 <tomy0000000@gmail.com>
2019-04-29 12:01:27 +0800

    Codecov Experiment #2

--- Tubee.py
+++ Tubee.py
@@ -46,7 +46,7 @@ def test(coverage):
         print("Coverage Summary:")
         COV.report()
         basedir = os.path.abspath(os.path.dirname(__file__))
-        covdir = os.path.join(basedir, "tmp/coverage")
+        covdir = os.path.join(basedir, "htmlcov")
         COV.html_report(directory=covdir)
         print("HTML version: file://%s/index.html" % covdir)
         COV.erase()
