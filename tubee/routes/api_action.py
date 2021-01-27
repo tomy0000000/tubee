dded08f4ca6362a289c7ad11f35ae9ce11c31783
Tomy Hsieh <tomy0000000@gmail.com>
2021-01-28 05:04:37 +0800

    Modify action API to compatible with group action

--- tubee/routes/api_action.py
+++ tubee/routes/api_action.py
@@ -11,21 +11,29 @@ api_action_blueprint = Blueprint("api_action", __name__)
 @login_required
 def new():
     form = ActionForm()
-    if form.validate_on_submit():
-        subscription = current_user.subscriptions.filter_by(
+    if not form.validate_on_submit():
+        abort(403)
+
+    if form.action_type.data == "Notification":
+        details = form.notification.data
+    elif form.action_type.data == "Playlist":
+        details = form.playlist.data
+    elif form.action_type.data == "Download":
+        details = form.download.data
+
+    if form.channel_id.data:
+        action_mixin = current_user.subscriptions.filter_by(
             channel_id=form.channel_id.data
         ).first_or_404()
-        if form.action_type.data == "Notification":
-            details = form.notification.data
-        elif form.action_type.data == "Playlist":
-            details = form.playlist.data
-        elif form.action_type.data == "Download":
-            details = form.download.data
-        response = subscription.add_action(
-            form.action_name.data, form.action_type.data, details
-        )
-        return jsonify(str(response))
-    abort(403)
+    elif form.tag.data:
+        action_mixin = current_user.tags.filter_by(name=form.tag.data).first_or_404()
+    else:
+        abort(403)
+
+    response = action_mixin.add_action(
+        form.action_name.data, form.action_type.data, details
+    )
+    return jsonify(str(response))
 
 
 @api_action_blueprint.route("/<action_id>")
@@ -33,7 +41,13 @@ def new():
 def action(action_id):
     """Get JSON of subscription action"""
     action = Action.query.filter_by(id=action_id).first_or_404()
-    if action.subscription not in current_user.subscriptions:
+    if action.subscription:
+        if action.subscription not in current_user.subscriptions:
+            abort(403)
+    elif action.tag:
+        if action.tag not in current_user.tags:
+            abort(403)
+    else:
         abort(403)
     return jsonify(
         dict(
@@ -49,20 +63,31 @@ def action(action_id):
 @login_required
 def delete(action_id):
     action = Action.query.filter_by(id=action_id).first_or_404()
-    if action.subscription not in current_user.subscriptions:
+    if action.subscription:
+        if action.subscription not in current_user.subscriptions:
+            abort(403)
+    elif action.tag:
+        if action.tag not in current_user.tags:
+            abort(403)
+    else:
         abort(403)
-    return jsonify(action.subscription.remove_action(action_id))
+    return jsonify(action.action_mixin.remove_action(action_id))
 
 
 @api_action_blueprint.route("/<action_id>", methods=["PATCH"])
 @login_required
 def edit(action_id):
     action = Action.query.filter_by(id=action_id).first_or_404()
+    if action.subscription:
+        if action.subscription not in current_user.subscriptions:
+            abort(403)
+    elif action.tag:
+        if action.tag not in current_user.tags:
+            abort(403)
+    else:
+        abort(403)
     form = ActionForm()
-    if (
-        action.subscription not in current_user.subscriptions
-        or not form.validate_on_submit()
-    ):
+    if not form.validate_on_submit():
         abort(403)
 
     modified = action.edit(form.data)
