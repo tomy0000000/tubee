173ebac263770fe6849e32f45d9963fe456b30ec
Tomy Hsieh <tomy0000000@gmail.com>
2021-06-13 16:16:56 +0800

    🛠 fix: Fix testing

--- tubee/models/__init__.py
+++ tubee/models/__init__.py
@@ -2,12 +2,13 @@
 
 Collect models for import
 """
-from .action import Action, ActionType  # noqa: F401
-from .callback import Callback  # noqa: F401
-from .channel import Channel  # noqa: F401
-from .notification import Notification, Service  # noqa: F401
-from .subscription import Subscription  # noqa: F401
-from .subscription_tag import SubscriptionTag  # noqa: F401
-from .tag import Tag  # noqa: F401
-from .user import User  # noqa: F401
-from .video import Video  # noqa: F401
+# flake8: noqa
+from .action import Action, ActionType
+from .callback import Callback
+from .channel import Channel
+from .notification import Notification, Service
+from .subscription import Subscription
+from .subscription_tag import SubscriptionTag
+from .tag import Tag
+from .user import User
+from .video import Video
