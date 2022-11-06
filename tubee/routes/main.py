a96a96b7cbafc631dde40732c0767aaa9a4f865c
Tomy Hsieh <tomy0000000@gmail.com>
2022-11-06 20:28:40 +0800

    ❇️ refactor: separate callback endpoint

--- tubee/routes/main.py
+++ tubee/routes/main.py
@@ -58,13 +58,9 @@ def channel(channel_id):
     )
 
 
-# TODO: REBUILD THIS
-@main_blueprint.route("/channel/<channel_id>/callback", methods=["GET", "POST"])
-def channel_callback(channel_id):
-    """
-    GET: Receive Hub Challenges to sustain subscription
-    POST: New Update from Hub
-    """
+@main_blueprint.get("/channel/<channel_id>/callback")
+def channel_callback_subscribe(channel_id):
+    """Receive Hub Challenges to sustain subscription"""
     channel_item = Channel.query.get_or_404(channel_id)
     callback_item = Callback(channel_item)
     infos = {
@@ -72,77 +68,86 @@ def channel_callback(channel_id):
         "arguments": request.args,
         "user_agent": str(request.user_agent),
     }
-    if request.method == "GET":
-        response = request.args.to_dict().get("hub.challenge")
-        if response:
-            callback_item.type = "Hub Challenge"
-            infos["details"] = response
-            callback_item.infos = infos
-        else:
-            response = callback_item.type = "Unknown GET Request"
-        db.session.commit()
-        return response
-    elif request.method == "POST":
+    response = request.args.to_dict().get("hub.challenge")
+    if response:
+        callback_item.type = "Hub Challenge"
+        infos["details"] = response
+        callback_item.infos = infos
+    else:
+        response = callback_item.type = "Unknown GET Request"
+    db.session.commit()
+    return response
 
-        # Preprocessing
-        infos["data"] = request.get_data(as_text=True)
-        soup = bs4.BeautifulSoup(infos["data"], "xml")
-        response = {}
-        # TODO: Inspecting
 
-        tag = soup.find("yt:videoId")
-        if tag is None:
-            current_app.logger.info(f"Video ID not Found for {callback_item}")
-            callback_item.infos = infos
-            db.session.commit()
-            return response
-        video_id = tag.string
+# TODO: REBUILD THIS
+@main_blueprint.post("/channel/<channel_id>/callback")
+def channel_callback(channel_id):
+    """Receive Hub Notifications"""
+    channel_item = Channel.query.get_or_404(channel_id)
+    callback_item = Callback(channel_item)
+    infos = {
+        "method": request.method,
+        "arguments": request.args,
+        "user_agent": str(request.user_agent),
+    }
 
-        # Update Database Records
-        video_item = Video.query.get(video_id)
-        new_video = not bool(video_item)
-        if new_video:
-            video_item = Video(video_id, channel_item)
-        video_item.callbacks.append(callback_item)
-        callback_item.video = video_item
-        callback_item.type = "Hub Notification"
+    # Preprocessing
+    infos["data"] = request.get_data(as_text=True)
+    soup = bs4.BeautifulSoup(infos["data"], "xml")
+    response = {}
+    # TODO: Inspecting
+
+    tag = soup.find("yt:videoId")
+    if tag is None:
+        current_app.logger.info(f"Video ID not Found for {callback_item}")
         callback_item.infos = infos
         db.session.commit()
+        return response
+    video_id = tag.string
 
-        # Pass actions if not new video
-        if not new_video:
-            return jsonify(response)
+    # Update Database Records
+    video_item = Video.query.get(video_id)
+    new_video = not bool(video_item)
+    if new_video:
+        video_item = Video(video_id, channel_item)
+    video_item.callbacks.append(callback_item)
+    callback_item.video = video_item
+    callback_item.type = "Hub Notification"
+    callback_item.infos = infos
+    db.session.commit()
 
-        # Fetch Video Infos
-        try:  # TODO
-            video_file_url = fetch_video_metadata(video_id)["url"]
-        except Exception:
-            video_file_url = None
-
-        # List users and execute actions
-        for sub in channel_item.subscriptions:
-            for action in sub.automated_actions:
-                try:
-                    results = action.execute(
-                        video_id=video_id,
-                        video_title=video_item.name,
-                        video_description=video_item.details["description"],
-                        video_thumbnails=video_item.details["thumbnails"]["medium"][
-                            "url"
-                        ],
-                        video_file_url=video_file_url,
-                        channel_name=channel_item.name,
-                    )
-                except Exception as error:
-                    results = {
-                        "error": error.__class__.__name__,
-                        "description": str(error),
-                    }
-                    current_app.logger.exception(f"{channel_id}-{action.id}: {error}")
-                else:
-                    current_app.logger.info(f"{channel_id}-{action.id}")
-                response[action.id] = results
+    # Pass actions if not new video
+    if not new_video:
         return jsonify(response)
+
+    # Fetch Video Infos
+    try:  # TODO
+        video_file_url = fetch_video_metadata(video_id)["url"]
+    except Exception:
+        video_file_url = None
+
+    # List users and execute actions
+    for sub in channel_item.subscriptions:
+        for action in sub.automated_actions:
+            try:
+                results = action.execute(
+                    video_id=video_id,
+                    video_title=video_item.name,
+                    video_description=video_item.details["description"],
+                    video_thumbnails=video_item.details["thumbnails"]["medium"]["url"],
+                    video_file_url=video_file_url,
+                    channel_name=channel_item.name,
+                )
+            except Exception as error:
+                results = {
+                    "error": error.__class__.__name__,
+                    "description": str(error),
+                }
+                current_app.logger.exception(f"{channel_id}-{action.id}: {error}")
+            else:
+                current_app.logger.info(f"{channel_id}-{action.id}")
+            response[action.id] = results
+    return jsonify(response)
 
 
 @main_blueprint.get("/video")
