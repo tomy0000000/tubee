7fd3570490e385ad3645407fb6dfeb483f3bd197
Tomy Hsieh <git@tomy.me>
2024-04-14 16:44:36 -0400

    ✨ feat: Drop `admin.channels` endpoint

--- tubee/routes/admin.py
+++ tubee/routes/admin.py
@@ -3,8 +3,6 @@ from flask import Blueprint, render_template
 from flask_login import login_required
 
 from .. import Tubee
-from ..models import Channel
-from ..tasks import list_all_tasks
 from ..utils import admin_required, build_sitemap
 
 current_app: Tubee
@@ -18,10 +16,3 @@ admin_blueprint.before_request(admin_required)
 def dashboard():
     links = build_sitemap()
     return render_template("admin/main.html", links=links, version=current_app.version)
-
-
-@admin_blueprint.get("/channels")
-def channels():
-    channels = Channel.query.all()
-    tasks = list_all_tasks()
-    return render_template("admin/channels_page.html", channels=channels, tasks=tasks)
